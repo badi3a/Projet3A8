9587556cda382cca30140483d8886bd1fb59b4d7
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-03-11 09:18:23 +0100

    session 7.1

---
+++ src/Controller/ClubController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Club;
+use App\Form\ClubType;
+use App\Repository\ClubRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/club")
+ */
+class ClubController extends AbstractController
+{
+    /**
+     * @Route("/", name="club_index", methods={"GET"})
+     */
+    public function index(ClubRepository $clubRepository): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'clubs' => $clubRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="club_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $club = new Club();
+        $form = $this->createForm(ClubType::class, $club);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($club);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('club_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('club/new.html.twig', [
+            'club' => $club,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="club_show", methods={"GET"})
+     */
+    public function show(Club $club): Response
+    {
+        return $this->render('club/show.html.twig', [
+            'club' => $club,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="club_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Club $club, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ClubType::class, $club);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('club_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('club/edit.html.twig', [
+            'club' => $club,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="club_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Club $club, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$club->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($club);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('club_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Student;
+use App\Form\StudentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,5 +35,30 @@ class StudentController extends AbstractController
         array(
             'list'=>$list
         ));
+    }
+    //add Student method
+    /**
+     * @Route("/new",name="newStudentPage")
+     */
+    public function newClassroom(Request $request):Response{
+        //1.Create form view
+        //1.a prepare an instance of the Student
+        $student= new Student();
+        //1.b prepare the form
+        $form= $this->createForm(StudentType::class, $student);
+        //2. Handel http request sent by the user
+        $form=$form->handleRequest($request);
+        //2.b check the form
+        if($form->isSubmitted() && $form->isValid()){
+            //3.Persist data
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($student);
+            $em->flush();
+            return $this->redirectToRoute("studentListPage");
+        }
+        //1.c render the form
+        return $this->render('student/new.html.twig',[
+            'f'=>$form->createView()
+        ]);
     }
 }
